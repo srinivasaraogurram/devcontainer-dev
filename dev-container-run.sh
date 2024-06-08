b7785c1acb81e28f7462890989aace477886ee8e
Srinivasa Rao Gurram <srinivasaraogurram@users.noreply.github.com>
2024-06-08 20:42:20 +0000

    adding multiple workspaces of dev container

---
+++ dev-container-run.sh
@@ -0,0 +1,9 @@
+npm install -g npm@10.8.1
+npm install -g @devcontainers/cli
+
+devcontainer up --workspace-folder << folder name >>
+
+devcontainer up --workspace-folder mslearn-python-products
+
+
+devcontainer up --workspace-folder vscode-remote-try-rust
